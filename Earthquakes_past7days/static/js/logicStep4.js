11dbdeaecdff5ec0357f8d82c3d196c67d48b9ef
Justin Malaussena <justin.malaussena0522@gmail.com>
2022-08-30 19:45:39 -0400

    toggle earthquake data

--- Earthquakes_past7days/static/js/logicStep4.js
+++ Earthquakes_past7days/static/js/logicStep4.js
@@ -118,10 +118,14 @@ L.geoJSON(data, {
       return L.circleMarker(latlng);
     },
   // We set the style for each circleMarker using our styleInfo function.
-style: styleInfo,
+  style: styleInfo,
   // We create a popup for each circleMarker to display the magnitude and
   //  location of the earthquake after the marker has been created and styled.
   onEachFeature: function(feature, layer) {
   layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
 }
-}).addTo(map)});
+}).addTo(earthquakes)
+
+  earthquakes.addTo(map);
+
+});
